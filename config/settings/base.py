372ccbc1dd0a75ef6164c1a4b5d3f7831091b322
Annjan <annjan7777@gmail.com>
2026-05-20 22:49:21 +0530

    fix: add AWS_S3_CUSTOM_DOMAIN and MEDIA_URL to base.py so file.url returns S3 URLs

--- config/settings/base.py
+++ config/settings/base.py
@@ -171,6 +171,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if AWS_ACCESS_KEY_ID:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+    MEDIA_ROOT = ''  # Not used when S3 is active
 
 
 def env_bool(name, default=False):
